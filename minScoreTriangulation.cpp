ccf9a7055d1da94d208f05785749bd720e9f4e5b
Abhishek Jha <69615011+Abhishekjha52@users.noreply.github.com>
2022-07-16 23:07:29 +0530

    Update minScoreTriangulation.cpp

--- minScoreTriangulation.cpp
+++ minScoreTriangulation.cpp
@@ -34,6 +34,24 @@ int solveMemo(vector<int>&values, int i, int j, vector<vector<int>>&dp)
     dp[i][j]=ans;
     return dp[i][j];
 }
+int solveTab(vector<int>&v)
+{
+    int n=v.size();
+    vector<vector<int>>dp(n, vector<int>(n, 0));
+    for(int i=n-1;i>=0;i--)
+    {
+        for(int j=i+2;j<n;j++)//we run loop from i+2 to ensure that we form triangle
+        {
+            int ans=INT_MAX;
+            for(int k=i+1;k<j;k++)
+            {
+                ans=min(ans, v[i]*v[j]*v[k] + dp[i][k] + dp[k][j]);
+            }
+            dp[i][j]=ans;
+        }
+    }
+    return dp[0][n-1];
+}
 int minScoreTriangulation(vector<int>& values) {
     int n=values.size();
     vector<vector<int>>dp(n, vector<int>(n, -1));
