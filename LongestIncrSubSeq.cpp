f3550b0a8993ce28e5cfb5dabb0137489c11f93e
Abhishek Jha <69615011+Abhishekjha52@users.noreply.github.com>
2022-07-26 19:18:07 +0530

    Update LongestIncrSubSeq.cpp

--- LongestIncrSubSeq.cpp
+++ LongestIncrSubSeq.cpp
@@ -49,12 +49,33 @@ int solveTab(vector<int>&a, int n)
     }
     return dp[0][0];
 }
+int solveOpt(vector<int>&a, int n)
+{
+    vector<int>currRow(n+1, 0);
+    vector<int>nextRow(n+1, 0);
+    for(int curr=n-1;curr>=0;curr--)
+    {
+        for(int prev=curr-1;prev>=-1;prev--)
+        {
+            int take=0;
+            if(prev==-1 || a[curr]>a[prev])
+                take=1+nextRow[curr+1];
+
+            int notTake=0+nextRow[prev+1];
+
+            currRow[prev+1]= max(take, notTake);
+        }
+        nextRow=currRow;
+    }
+    return nextRow[0];
+}
 int lengthOfLIS(vector<int>& nums) {
     int n=nums.size();
     //return solveRec(nums, n, 0, -1);
     //vector<vector<int>>dp(n, vector<int>(n+1, -1));
     //return solveMemo(nums, n, 0, -1, dp);
-    return solveTab(nums, n);
+    //return solveTab(nums, n);
+    return solveOpt(nums, n);
 }
 int main()
 {
